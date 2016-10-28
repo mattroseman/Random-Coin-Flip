f934e34952cd237b7f4b064917de245efbd51b28
Matthew Roseman <mrr77@case.edu>
2016-10-28 13:14:49 -0400

    now writes score to log file when program is exited, and resumes where left off
    Signed-off-by: Matthew Roseman <mrr77@case.edu>

--- coin_flip.py
+++ coin_flip.py
@@ -1,7 +1,27 @@
-import struct
-import time
+#!/usr/bin/python3
+import os, sys, struct, time, re
+
+log_name = 'log.txt'
 
 _random = open("/dev/random", "rb")
+
+bit_mask = 1 #  00000000 00000000 00000000 00000001
+heads = 0
+tails = 0
+
+if os.path.exists(log_name):
+    log = open(log_name, 'r+')
+else:
+    print ('{} doesn\'t exist so one is being created'.format(log_name))
+    log = open(log_name, 'w')
+match = re.search('^heads=(\d+) tails=(\d+)$', log.readline())
+if match:
+    heads = int(match.group(1))
+    tails = int(match.group(2))
+else:
+    print ('{} was formatted incorrectly'.format(log_name))
+log.seek(0)
+log.truncate()
 
 def uint32_to_int(bin_num):
     """
@@ -12,36 +32,38 @@ def uint32_to_int(bin_num):
     return struct.unpack("I", bin_num)[0]
 
 def main():
+    global heads, tails, bit_mask, _random, log_name
 
-    bit_mask = 1 #  00000000 00000000 00000000 00000001
-    heads = 0
-    tails = 0
+    try:
+        #  run until user kills process
+        while True:
+            #  generates 4 bytes or an unsigned int
+            rand_bytes = _random.read(4)
+            #  converts bytes to integer
+            rand_int = uint32_to_int(rand_bytes)
+            #  print heads for every 1 bit and tails for every 0 bit
+            for i in range(32):
 
-    #  run until user kills process
-    while True:
-
-        #  generates 4 bytes or an unsigned int
-        rand_bytes = _random.read(4)
-        #  converts bytes to integer
-        rand_int = uint32_to_int(rand_bytes)
-        #  print heads for every 1 bit and tails for every 0 bit
-        for i in range(32):
-
-            #   xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
-            #  &00000000 00000000 00000000 00000001
-            #  ------------------------------------
-            #   00000000 00000000 00000000 0000000x
-            masked_int = rand_int & bit_mask
-            print ("", end="\r")
-            if masked_int == 1:
-                heads += 1
-            else:
-                tails += 1
-            rand_int = rand_int >> 1
-            print ("flips: {} | heads: {} | tails: {} | percent heads: {}".format(heads
-                + tails, heads, tails,
-                heads / (heads + tails)), end="")
-            time.sleep(.5)
+                #   xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+                #  &00000000 00000000 00000000 00000001
+                #  ------------------------------------
+                #   00000000 00000000 00000000 0000000x
+                masked_int = rand_int & bit_mask
+                print ("", end="\r")
+                if masked_int == 1:
+                    heads += 1
+                else:
+                    tails += 1
+                rand_int = rand_int >> 1
+                print ("flips: {} | heads: {} | tails: {} | percent heads: {}".format(heads
+                    + tails, heads, tails,
+                    heads / (heads + tails)), end="")
+                time.sleep(.5)
+    except (KeyboardInterrupt, SystemExit):
+        print ('\nwriting current score to {}'.format(log_name))
+        log.write('heads={} tails={}'.format(heads, tails))
+        log.close()
+        sys.exit()
 
 if __name__ == "__main__":
     main()
